72c7ddd6ade6f31f36b912be9663d4110db99b8c
yaw precious <yawprecious072@gmail.com>
2022-09-25 09:43:44 -0700

    added param middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ const tourController = require('../controllers/tourController')
 //ROUTES
 const router = express.Router();
 
+router.param('id',tourController.checkID)
 
    router.route('/').get(tourController.getAllTours).post(tourController.createTour);
    router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
