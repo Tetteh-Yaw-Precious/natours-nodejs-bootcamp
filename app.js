f86a2b7142ee9b868889b8a1adf108dd8db02866
Yaw Precious <yawprecious072@gmail.com>
2022-09-28 17:10:44 +0000

    added middleware to request body

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
-const userRouter = require("./routes/userRoutes")
+const userRouter = require('./routes/userRoutes');
 
 //MIDDLEWARES
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-app.use((req,res,next)=>{
-  req.requestTime = new Date().toISOString()
-  next()
-})
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
-app.use('/api/v1/tours',tourRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.param('id',tourController.checkID)
 
-   router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+//middleware to check if request body contains name and price
+
+   router.route('/').get(tourController.getAllTours).post(tourController.checkBody ,tourController.createTour);
    router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
    
 module.exports = router;
